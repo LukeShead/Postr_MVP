a2c3d05328798d093bb296b17e4a7f82419fa89a
Conor Bailey <cpbailey025@gmail.com>
2023-06-26 22:01:33 +0100

    struggling to get the post function to work

--- Conor's Tests/post_bot.py
+++ Conor's Tests/post_bot.py
@@ -5,13 +5,25 @@ api_secrets = ""
 access_token = ""
 access_secret = ""
 
+bearer_token = ""
+
 # Authenticate to Twitter
-auth = tweepy.OAuth1UserHandler(
-   api_key, api_secrets,
-   access_token, access_secret
+#auth = tweepy.OAuth1UserHandler(
+auth = tweepy.OAuth2AppHandler(
+   consumer_key=api_key,
+   consumer_secret=api_secrets
+   #bearer_token
+   )
+
+client = tweepy.Client(
+    api_key,
+    api_secrets,
+    access_token,
+    access_secret
 )
  
-api = tweepy.API(auth)
+api = tweepy.API(client)
 
 tweet = "This is my first tweet using Tweepy"
+#client.create_tweet(tweet)
 api.update_status(status=tweet)
