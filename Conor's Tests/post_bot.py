d62feb98fddf54ce8f86900e713e673b0e9cd852
Conor Bailey <cpbailey025@gmail.com>
2023-06-27 19:04:13 +0100

    tidied up

--- Conor's Tests/post_bot.py
+++ Conor's Tests/post_bot.py
@@ -1,15 +1,23 @@
 import tweepy
+import json, os
 
-api_key = ""
-api_secret = ""
-access_token = ""
-access_secret = ""
+#Getting the API keys from external file (not on Git)
+this_file = os.path.dirname(__file__)
+f = open(this_file + "/api_keys.json")
+data = json.load(f)
 
-bearer_token = ""
+#Literally all the keys one could possibly need
+api_key = data['api_key']
+api_secret = data['api_secret']
+access_token = data['access_token']
+access_secret = data['access_secret']
 
-client_ID = ""
-client_secret = ""
+bearer_token = data['bearer_token']
 
+client_ID = data['client_ID']
+client_secret = data['client_secret']
+
+#Creating API client
 client = tweepy.Client(
     consumer_key=api_key,
     consumer_secret=api_secret,
@@ -17,5 +25,6 @@ client = tweepy.Client(
     access_token_secret=access_secret
 )
 
+#Posting tweet
 tweet = "This is my first tweet using Tweepy"
 client.create_tweet(text=tweet)
